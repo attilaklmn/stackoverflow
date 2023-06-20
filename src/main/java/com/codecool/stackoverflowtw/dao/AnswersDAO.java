2ff4ec27f3fb280dc5f6357389cd46bef1479578
Szajkoggg <116949920+Szajkoggg@users.noreply.github.com>
2023-06-20 14:53:10 +0200

    add method for getting all answers for a question id

--- src/main/java/com/codecool/stackoverflowtw/dao/AnswersDAO.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/AnswersDAO.java
@@ -1,4 +1,9 @@
 package com.codecool.stackoverflowtw.dao;
 
+import com.codecool.stackoverflowtw.dao.model.Answer;
+
+import java.util.List;
+
 public interface AnswersDAO {
+    List<Answer> getAllAnswersForQuestionID(int questionId);
 }
--- src/main/java/com/codecool/stackoverflowtw/dao/AnswersDaoJdbc.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/AnswersDaoJdbc.java
@@ -1,14 +1,46 @@
 package com.codecool.stackoverflowtw.dao;
 
+import com.codecool.stackoverflowtw.dao.model.Answer;
+import com.codecool.stackoverflowtw.dao.model.Question;
 import com.codecool.stackoverflowtw.sql.database.Database;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnswersDaoJdbc implements AnswersDAO{
     private final Database database;
-
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     public AnswersDaoJdbc(Database database) {
         this.database = database;
+    }
+
+    @Override
+    public List<Answer> getAllAnswersForQuestionID(int questionId) {
+        String query = "SELECT * FROM answer WHERE question_id = ?";
+        try (Connection connection = database.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, questionId);
+            ResultSet resultSet = statement.executeQuery(query);
+                List<Answer> allQuestions = new ArrayList<>();
+                while (resultSet.next()) {
+                    allQuestions.add(getAllAnswerDetailsFromResultSet(resultSet));
+                }
+                return allQuestions;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private Answer getAllAnswerDetailsFromResultSet(ResultSet resultSet) {
+        try {
+            return new Answer(resultSet.getInt("id"), resultSet.getString("answer"), resultSet.getInt("question_id"), resultSet.getTimestamp("created").toLocalDateTime(), resultSet.getString("username"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
