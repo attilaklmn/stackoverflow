91931faf3e98a019a434204b224ec31c66f0ef7d
Szajkoggg <116949920+Szajkoggg@users.noreply.github.com>
2023-06-21 16:37:13 +0200

    change DTO parameter names

--- src/main/java/com/codecool/stackoverflowtw/controller/dto/AnswerDTO.java
+++ src/main/java/com/codecool/stackoverflowtw/controller/dto/AnswerDTO.java
@@ -2,5 +2,5 @@ package com.codecool.stackoverflowtw.controller.dto;
 
 import java.time.LocalDateTime;
 
-public record AnswerDTO(int id, String answer, int question_id, LocalDateTime createDate, String username) {
+public record AnswerDTO(int id, String answer, int questionId, LocalDateTime createDate, String userName) {
 }
