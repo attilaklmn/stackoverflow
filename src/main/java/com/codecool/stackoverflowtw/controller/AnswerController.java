76dd251e3901c360ce4f2c1dee7d0376f1031bb1
Szajkoggg <116949920+Szajkoggg@users.noreply.github.com>
2023-06-21 11:17:09 +0200

    add paths for answers

---
+++ src/main/java/com/codecool/stackoverflowtw/controller/AnswerController.java
@@ -0,0 +1,39 @@
+package com.codecool.stackoverflowtw.controller;
+
+import com.codecool.stackoverflowtw.controller.dto.AnswerDTO;
+import com.codecool.stackoverflowtw.controller.dto.NewAnswerDTO;
+import com.codecool.stackoverflowtw.controller.dto.NewQuestionDTO;
+import com.codecool.stackoverflowtw.controller.dto.QuestionDTO;
+import com.codecool.stackoverflowtw.dao.model.Answer;
+import com.codecool.stackoverflowtw.service.AnswerService;
+import com.codecool.stackoverflowtw.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("answers")
+public class AnswerController {
+    private final AnswerService answerService;
+
+    @Autowired
+    public AnswerController(AnswerService answerService) {
+        this.answerService = answerService;
+    }
+
+    @GetMapping("/{id>")
+    public List<AnswerDTO> getAllAnswersForQuestionID(@PathVariable int id) {
+        return answerService.getAllAnswersForQuestionID(id);
+    }
+
+    @PostMapping("/new")
+    public int addNewAnswer(@RequestBody NewAnswerDTO answerDTO) {
+        return answerService.addNewAnswer(answerDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteAnswerById(@PathVariable int id) {
+        return answerService.deleteAnswer(id);
+    }
+}
