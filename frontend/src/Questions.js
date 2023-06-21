3bfd3d48ef3e96f5f79949899ab1b1360f09b5f5
attilaklmn <kalmanattila04@gmail.com>
2023-06-21 11:02:29 +0200

    add dummy questions to question cards

--- frontend/src/Questions.js
+++ frontend/src/Questions.js
@@ -1,13 +1,47 @@
+import NewQuestionCard from "./components/NewQuestionCard";
 import QuestionCard from "./components/QuestionCard";
 import { Container } from "@mui/material";
 
+const DUMMY_QUESTIONS = [
+  {
+    id: 1,
+    title: "title1",
+    desc: "description 1",
+    createDate: "create date",
+    userName: "user1",
+  },
+  {
+    id: 2,
+    title: "title2",
+    desc: "description 2",
+    createDate: "create date",
+    userName: "user2",
+  },
+  {
+    id: 3,
+    title: "title3",
+    desc: "description 3",
+    createDate: "create date",
+    userName: "user3",
+  },
+];
+
 const Questions = () => {
-    return (
-        <Container sx={{ display:'flex', alignItems:'center', justifyContent:'center', flexDirection:'column' }}>
-        <QuestionCard userName="user1" />
-        <QuestionCard userName="user2" />
-        </Container>
-    )
-}
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <NewQuestionCard />
+      {DUMMY_QUESTIONS.map((e) => {
+        return <QuestionCard question={e} />;
+      })}
+    </Container>
+  );
+};
 
 export default Questions;
---
+++ frontend/src/components/AnswerCard.js
@@ -0,0 +1,12 @@
+import { Card, CardHeader } from "@mui/material";
+
+const AnswerCard = props => {
+    return (
+        <Card sx={{ margin: 1 }}>
+            <CardHeader title="username" subheader="date" />
+            answer1
+          </Card>
+    )
+}
+
+export default AnswerCard;
